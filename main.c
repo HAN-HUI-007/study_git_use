badfa6ede4b8b24669a629d60dd6f2c10a7c195e
HAN-HUI-007 <HANHUI20230921@163.COM>
2026-01-29 18:45:41 +0800

    增加main.c

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello World!\n");
+    printf("Hello World!\n");
+    printf("Hello World!\n");
+    printf("Hello World!\n");
+    printf("Hello World!\n");
+    printf("Hello World!\n");
+    printf("Hello World!\n");
+    printf("Hello World!\n");
+    printf("Hello World!\n");
+    while (1)
+    {
+        /* code */
+        printf("Hello World!\n");
+    }
+    
+    return 0;
+}
