e8380d79b2e40b99ee5d4610950b171640ad6ba4
HAN-HUI-007 <HANHUI20230921@163.COM>
2026-01-30 15:32:38 +0800

    修改逻辑

--- main.c
+++ main.c
@@ -15,6 +15,21 @@ int main()
     {
         /* code */
         printf("Hello World!\n");
+        printf("Hello World!\n");
+        while (1)
+        {
+            /* code */
+            printf("Hello World!\n");
+            printf("Hello World!\n");
+            printf("Hello World!\n");
+            printf("Hello World!\n");
+            printf("Hello World!\n");
+            printf("Hello World!\n");
+            printf("Hello World!\n");
+            printf("Hello World!\n");
+            printf("Hello World!\n");
+            printf("Hello World!\n");
+        }
     }
     
     return 0;
